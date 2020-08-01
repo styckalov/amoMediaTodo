c62234c439fd88860c22afa29ff18899fcc44266
styckalov <mega.yolo@ukr.net>
2020-08-01 12:17:12 +0300

    fix heroku build

---
+++ public/precache-manifest.c48d89e2c74ac705cfc7d85674953190.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de70a138fcfda1603efe96c4d759fc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63dd18a6d8edff104f0b",
+    "url": "/static/css/2.291b60a3.chunk.css"
+  },
+  {
+    "revision": "e800a3af1760e03a4f42",
+    "url": "/static/css/main.aeab71ca.chunk.css"
+  },
+  {
+    "revision": "63dd18a6d8edff104f0b",
+    "url": "/static/js/2.a56a37fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a56a37fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e800a3af1760e03a4f42",
+    "url": "/static/js/main.edb85688.chunk.js"
+  },
+  {
+    "revision": "6af0f0b01a8fa4b15947",
+    "url": "/static/js/runtime-main.7e3a2173.js"
+  },
+  {
+    "revision": "6a987a9bed0e6fda14a7077fdf946b65",
+    "url": "/static/media/caveat-latin-400.6a987a9b.woff2"
+  },
+  {
+    "revision": "f7a87f4452956224b53807ba65532488",
+    "url": "/static/media/caveat-latin-400.f7a87f44.woff"
+  },
+  {
+    "revision": "0aab84612801ff2dfd1f5b2ac9ce9df6",
+    "url": "/static/media/caveat-latin-700.0aab8461.woff2"
+  },
+  {
+    "revision": "97bf925fd7b96801ffc629143cbd8a22",
+    "url": "/static/media/caveat-latin-700.97bf925f.woff"
+  }
+]);
